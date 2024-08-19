8f6a994eb1ee3b047a403efb41b3ac6b150529c8
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-19 15:20:57 -0700

    fixed width issue on mobile for submit button

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -8,16 +8,16 @@ export default function SubmitButton() {
   return (
     <button
       type="submit"
-      className="group bg-gray-900 dark:bg-white/10 text-white px-7 py-3 flex items-center h-[3rem] w-[9.8rem] gap-2 rounded-full outline-none transition focus:bg-gray-700 hover:bg-gray-700 active:bg-gray-600 dark:focus:bg-gray-800 dark:hover:bg-gray-800 dark:active:bg-gray-700"
+      className="group bg-gray-900 dark:bg-white/10 text-white px-7 py-3 gap-2 rounded-full outline-none transition focus:bg-gray-700 hover:bg-gray-700 active:bg-gray-600 dark:focus:bg-gray-800 dark:hover:bg-gray-800 dark:active:bg-gray-700 max-w-max"
       disabled={pending}
     >
       {pending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white mx-auto"></div>
       ) : (
-        <>
+        <p className="flex items-center gap-x-2">
           Submit{" "}
           <Send size={20} className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-        </>
+        </p>
       )}
     </button>
   );
