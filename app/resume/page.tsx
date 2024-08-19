9b64ec7871e49ed19a489c73990170228f464f0c
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-19 15:10:03 -0700

    check if redirect is successful

---
+++ app/resume/page.tsx
@@ -0,0 +1,6 @@
+
+import { redirect } from 'next/navigation'
+ 
+export default async function Profile() {
+    redirect('https://drive.google.com/file/d/10VLnSjS4zkrElxx05xL1y7xiBI9UgPls/view?usp=sharing')
+}
