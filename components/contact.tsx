4d8bff82b9a00c0dc28b49235458c10035004e59
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-08-18 18:53:20 -0700

    small cleanup

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,15 +5,18 @@ import { useToast } from "./ui/use-toast";
 import { useRef } from "react";
 
 const Contact = () => {
-  const {toast} = useToast();
+  const { toast } = useToast();
   const formRef = useRef<HTMLFormElement>(null);
   return (
     <section
       id="contact"
       aria-label="Contact"
       className="mb-20 sm:mb-28 w-full max-w-screen-lg mx-auto px-4 sm:px-8 md:px-10 sm:text-2xl">
-      <p className="leading-normal text-xs text-white/70">
-        Please contact me directly at{" "}
+      <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl">
+        CONTACT 
+      </h1>
+      <p className="leading-normal text-xs text-white/70 mb-6">
+        Email me directly at{" "}
         <a className="underline" href="mailto:ahir.ankur0429@gmail.com">
           ahir.ankur0429@gmail.com
         </a>{" "}
@@ -29,15 +32,15 @@ const Contact = () => {
             toast({
               variant: "destructive",
               title: "Error occurred on my end.",
-              description: "please send me the email directly"
-            })
+              description: "please send me the email directly",
+            });
             return;
           }
           toast({
             variant: "default",
             title: "Success",
-            description: "I'll will be in touch soon!"
-          })
+            description: "I'll will be in touch soon!",
+          });
           formRef.current?.reset();
         }}>
         <input
--- components/technologies.tsx
+++ components/technologies.tsx
@@ -2,15 +2,15 @@ import Skills from "./skills";
 
 const Technologies = () => {
   return (
-    <div className="mb-20">
-      <div className="w-full max-w-screen-lg mx-auto px-4 sm:px-8 md:px-10 sm:text-2xl mb-10">
-        <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl">
-          TECHNOLOGIES
-        </h1>
-        <blockquote className="mt-6 border-l-2 pl-6 italic leading-relaxed text-white/70">
-          My focus is on developing accessible web applications and automated deployment pipelines. I work with technologies that service business needs.
-        </blockquote>
-      </div>
+    <div className="w-full max-w-screen-lg mx-auto px-4 sm:px-8 md:px-10 sm:text-2xl mb-20">
+      <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl">
+        TECHNOLOGIES
+      </h1>
+      <blockquote className="mt-6 border-l-2 pl-6 italic leading-relaxed text-white/70 mb-6">
+        My focus is on developing accessible web applications and automated
+        deployment pipelines. I work with technologies that service business
+        needs.
+      </blockquote>
       <Skills />
     </div>
   );
